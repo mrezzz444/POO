aca32f1c49e4e132b895d0ffc8299e15419c530d
Isaac Avila <isaac.avila.saenz29@gmail.com>
2024-08-27 10:51:55 -0600

    clase del 27 de agosto

--- Algoritmos/Primera unidad/PracticaMatrices/main.cpp
+++ Algoritmos/Primera unidad/PracticaMatrices/main.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
 #include "src\Matriz.cpp"
+
 using namespace std;
-
-bool t_usuario(unsigned short int &, unsigned short int &); //Parametros por referencia
+bool tamanio(unsigned short int &, unsigned short int &); //Parametros por referencia
 unsigned short int menu();
-
-
-
+void operaciones(void);
+void mensaje(char);
 int main()
 {
     unsigned short int eleccion, filas, columnas;
@@ -20,7 +19,7 @@ int main()
     return 0;
 }
 
-bool t_usuario(unsigned short int &fil, unsigned short int &col)
+bool tamanio(unsigned short int &fil, unsigned short int &col)
 {
     cout <<"Teclea las filas ";
     cin >> fil;
@@ -35,6 +34,7 @@ bool t_usuario(unsigned short int &fil, unsigned short int &col)
     // return false;
 }
 
+//Presenta el menu de opciones
 unsigned short int menu()
 {
     unsigned short int opcion;
@@ -46,12 +46,90 @@ unsigned short int menu()
         cout << "1. Leer tamaño" << endl;
         cout << "2. Suma" << endl;
         cout << "3. Resta" << endl;
-        cout << "4. MUltiplicacion" << endl;
+        cout << "4. Multiplicacion" << endl;
         cout << "5. Salir" << endl;
         cin >> opcion;
-
 
     }while(opcion <1 || opcion > 5);
     
     return opcion;
+}
+
+void operaciones(void)
+{
+    unsigned short int fil, col, opcion;
+    bool centinela = false;
+    opcion = menu();
+
+    do{
+
+    }while(opcion);
+    {
+    switch (opcion)
+    {
+           case 1:
+                cout <<"Llenando tamaño" << endl;
+
+                if (tamanio(fil, col))
+                {
+                    centinela = true;
+                    Matriz mat1(col, fil), mat2(col,fil), mat3(col,fil);
+                }
+                else  
+                {
+                    mensaje();
+                    centinela = false;            
+                    break;
+                }  
+                   
+
+                break;
+            
+            case 2:
+                
+                if(centinela){
+                    //Sumar la matriz
+                    mat1.SumatMatriz(mat2, resul);
+                    
+
+                    mensaje();
+                }else{
+
+                }
+
+                break;
+            
+            case 3:
+                if(centinela){
+                    mensaje();
+                }else{
+
+                }
+
+                break;
+            
+            case 4:
+                if(centinela){
+                    mensaje();
+                }else{
+
+                }
+
+                break;
+            
+            case 5:
+                
+            
+            default:
+                // Code for default case
+                break;
+     }
+    };
+  
+}
+
+void mensaje(char valor)
+{
+    if(valor == "1")
+    cout <<"Error, algo salio mal" << endl;
 }
