ee6cd9d5dfe290f2e517cc98f4f722a38bf6957c
Isaac Avila <isaac.avila.saenz29@gmail.com>
2024-09-09 18:19:38 -0600

    tarea: completar el programa con todas las operaciones

--- Algoritmos/Primera unidad/PracticaMatrices/main.cpp
+++ Algoritmos/Primera unidad/PracticaMatrices/main.cpp
@@ -2,154 +2,121 @@
 #include "src\Matriz.cpp"
 
 using namespace std;
-bool tamanio(unsigned short int &, unsigned short int &); //Parametros por referencia
+bool tamanio(unsigned short int &, unsigned short int &); // A estas alturas no es necesario poner nombre a las variables
 unsigned short int menu();
-void operaciones(void);
+void operaciones(Matriz, Matriz, Matriz &);
 void mensaje(char);
+
 int main()
 {
-    unsigned short int eleccion, filas, columnas;
-    do{
-        eleccion = menu();
-    }while(eleccion !=5);
+    unsigned short int fil, col;
+    setlocale(LC_ALL, "");
+    cout << "Creando arreglos" << endl;
+    if (tamanio(fil, col))
+    {
+        // Crear los arreglos y se recorren los constructores
+        Matriz mat1(col, fil), mat2(col, fil), resul(col, fil);
+        operaciones(mat1, mat2, resul);
+    }
+    else
+        mensaje(0);
 
-    // cout<<"Hello World";
-    unsigned short int menu();
-    
+    cout << "Hola mundo!" << endl;
     return 0;
 }
-
+// pide y guarda el tamaño de la matriz
 bool tamanio(unsigned short int &fil, unsigned short int &col)
 {
-    cout <<"Teclea las filas ";
+    cout << "Teclee las filas: ";
     cin >> fil;
-    cout <<"Teclea las columnas";
+    cout << "Teclee las columnas: ";
     cin >> col;
-
-    return  (fil >= 2 and fil <=10) and (col >= 2 and col <=10);
-
-    //optimizando codigo aparentemente 
-
-    // if((fil >= 2 and fil <=10) and (col >= 2 and col <=10)) return true;
-    // return false;
+    return (fil >= 2 && fil <= 10) && (col >= 2 && col <= 10);
 }
 
-//Presenta el menu de opciones
+// presenta menu de opciones
 unsigned short int menu()
 {
     unsigned short int opcion;
-
-    do{
-
+    setlocale(LC_ALL, "");
+    do
+    {
         system("CLS");
-        cout <<"********** Menú de opciones **********" << endl;
-        cout << "1. Leer tamaño" << endl;
-        cout << "2. Suma" << endl;
-        cout << "3. Resta" << endl;
-        cout << "4. Multiplicacion" << endl;
-        cout << "5. Salir" << endl;
-        cin >> opcion;
+        cout << "****** Menú de opciones ******** " << endl;
 
-    }while(opcion <1 || opcion > 5);
-    
+        cout << "1) Suma " << endl;
+        cout << "2) Resta " << endl;
+        cout << "3) Multiplicación " << endl;
+        cout << "4) Salir " << endl;
+        cin >> opcion;
+    } while (opcion < 1 || opcion > 5);
     return opcion;
 }
 
-void operaciones(void)
+// Resuelve dentro de una estructura switch las operaciones
+void operaciones(Matriz mat1, Matriz mat2, Matriz &resul)
 {
-    unsigned short int fil, col, opcion;
-    bool centinela = false;
-    Matriz mat1(0, 0), mat2(0, 0), mat3(0, 0), resul(0,0); // Declarar matrices aquí para tener acceso en todos los casos
-    do {
+    unsigned short int opcion;
+
+    setlocale(LC_ALL, "");
+    do
+    {
         opcion = menu();
-        switch (opcion) {
-            case 1:
-                cout << "Llenando tamaño" << endl;
+        switch (opcion)
+        {
 
-                if (tamanio(fil, col)) {
-                    centinela = true;
-                    //Creando los objetos y se corren los contructores
-                    mat1 = Matriz(fil, col); // Creamos las matrices con el tamaño especificado
-                    mat2 = Matriz(fil, col);
-                    resul = Matriz(fil, col); // Matriz para resultados
-                    cout << "Matrices creadas con éxito." << endl;
-                } else {
-                    mensaje(0);
-                    centinela = false;
-                }
-                break;
+        case 1:
+            if (mat1.SumarMatriz(mat2, resul))
+            {
+                mensaje(1);
+                mat1.Imprimir();
+                mat2.Imprimir();
+                resul.Imprimir();
+                system("PAUSE");
+            }
+            else
+                mensaje(0);
 
-            case 2:
-                if (centinela) {
-                    // Sumar las matrices
-                    if (mat1.SumarMatriz(mat2, resul)) {
-                        mensaje(1);
-                        cout << "Matriz 1:" << endl;
-                        mat1.Imprimir();
-                        cout << "Matriz 2:" << endl;
-                        mat2.Imprimir();
-                        cout << "Resultado de la suma:" << endl;
-                        resul.Imprimir();
-                    } else {
-                        mensaje(0);
-                    }
-                } else {
-                    mensaje(0);
-                }
-                break;
+            break;
+        case 2:
+            if (mat1.RestarMatriz(mat2, resul))
+            {
+                mensaje(1);
+                mat1.Imprimir();
+                mat2.Imprimir();
+                resul.Imprimir();
+            }
+            else
+                mensaje(0);
 
-            case 3:
-                if (centinela) {
-                    // Restar las matrices
-                    if (mat1.RestarMatriz(mat2, resul)) {
-                        mensaje(1);
-                        cout << "Matriz 1:" << endl;
-                        mat1.Imprimir();
-                        cout << "Matriz 2:" << endl;
-                        mat2.Imprimir();
-                        cout << "Resultado de la resta:" << endl;
-                        resul.Imprimir();
-                    } else {
-                        mensaje(0);
-                    }
-                } else {
-                    mensaje(0);
-                }
-                break;
+            system("PAUSE");
+            break;
+        case 3:
+            if (mat1.MultiplicarMatriz(mat2, resul))
+            {
+                mensaje(1);
+                mat1.Imprimir();
+                mat2.Imprimir();
+                resul.Imprimir();
+            }
+            else
+                mensaje(0);
+            system("PAUSE");
 
-            case 4:
-                if (centinela) {
-                    // Multiplicar las matrices
-                    if (mat1.MultiplicarMatriz(mat2, resul)) {
-                        mensaje(1);
-                        cout << "Matriz 1:" << endl;
-                        mat1.Imprimir();
-                        cout << "Matriz 2:" << endl;
-                        mat2.Imprimir();
-                        cout << "Resultado de la multiplicación:" << endl;
-                        resul.Imprimir();
-                    } else {
-                        mensaje(0);
-                    }
-                } else {
-                    mensaje(0);
-                }
-                break;
-
-            case 5:
-                cout << "Saliendo del programa..." << endl;
-                break;
+            break;
+        case 4:
+            cout << "Saliendo del programa..." << endl;
+            system("PAUSE");
         }
-    } while (opcion != 5);
-}
 
+    } while (opcion != 4);
+}
 
 void mensaje(char valor)
 {
-    if(1){
-        cout <<"Operacion realizada " << endl;
-    }
-    else{
-    cout <<"Error, algo salio mal" << endl;
-    }
+    setlocale(LC_ALL, "");
+    if (1)
+        cout << "Operación realizada" << endl;
+    else
+        cout << "el tamaño no permite la operación" << endl;
 }
--- Algoritmos/Primera unidad/PracticaMatrices/src/Matriz.cpp
+++ Algoritmos/Primera unidad/PracticaMatrices/src/Matriz.cpp
@@ -1,66 +1,87 @@
 #include "Matriz.h"
-#include <bits/stdc++.h> //Biblioteca que contiene casi todas las librerias estandar
+#include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
 inline void Matriz::llenarMatriz()
 {
-	//Se tiene que generar la semilla para no repetir los numeros aleatorios
-	for(int fil = 0; fil<fila; fil++)
-	{
-		for(int col = 0; col<columna; col++)
-		{
-			elementos[fil][col] = rand()%100 + 1; //rango de 1 a 100
-		}
-	}
-}
-
-inline bool Matriz:: SumarMatriz(Matriz matB, Matriz& matC) {
-	if ((fila != matB.fila) or (columna != matB.columna))
-		return false;
-		
-	for(int fil = 0; fil<fila; fil++)
-	{
-		for(int col = 0; col<columna; col++)
-		{
-			cout << elementos[fil][col]  << " " ;
-
-			//return (fil == fila && col == columna) ? true : false;
-			return (fil == fila && col == columna);
-
-
-		}
-
-
-	}
-
-
-}//funcion para sumar matrices, obviamente u.u
-inline bool Matriz::RestarMatriz(Matriz matB, Matriz& matC) {
-    if ((fila != matB.fila) || (columna != matB.columna))
-        return false;
-    
-    for(int fil = 0; fil < fila; fil++) {
-        for(int col = 0; col < columna; col++) {
-            matC.elementos[fil][col] = elementos[fil][col] - matB.elementos[fil][col];
+    srand(time(0)); // Inicializar la semilla
+    for (int fil = 0; fil < fila; fil++)
+    {
+        for (int col = 0; col < columna; col++)
+        {
+            elementos[fil][col] = rand() % 100 + 1; // Números aleatorios de 1 a 100
         }
     }
-    
+}
+
+inline bool Matriz::SumarMatriz(Matriz matB, Matriz &matC)
+{
+    if (fila != matB.fila || columna != matB.columna)
+        return false;
+
+    for (int fil = 0; fil < fila; fil++)
+    {
+        for (int col = 0; col < columna; col++)
+        {
+            matC.elementos[fil][col] = elementos[fil][col] + matB.elementos[fil][col];
+        }
+    }
     return true;
 }
 
+inline bool Matriz::RestarMatriz(Matriz matB, Matriz &matC)
+{
+    if (fila != matB.fila || columna != matB.columna)
+        return false;
 
-inline bool Matriz::MultiplicarMatriz(Matriz, Matriz&) {}
+    for (int fil = 0; fil < fila; fil++)
+    {
+        for (int col = 0; col < columna; col++)
+        {
+            matC.elementos[fil][col] = elementos[fil][col] - matB.elementos[fil][col];
+        }
+    }
+    return true;
+}
+
+inline bool Matriz::MultiplicarMatriz(Matriz matB, Matriz &matC)
+{
+    if (columna != matB.fila) // Condición para multiplicar
+        return false;
+
+    for (int i = 0; i < fila; i++)
+    {
+        for (int j = 0; j < matB.columna; j++)
+        {
+            matC.elementos[i][j] = 0;
+            for (int k = 0; k < columna; k++)
+            {
+                matC.elementos[i][j] += elementos[i][k] * matB.elementos[k][j];
+            }
+        }
+    }
+    return true;
+}
 
 inline void Matriz::Imprimir()
 {
-	for(int fil = 0; fil<fila; fil++)
-	{
-		for(int col = 0; col<columna; col++)
-		{
-			cout << elementos[fil][col]  << " " ;
-		}
-		cout <<endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
 
-	}
+    for (int fil = 0; fil < fila; fil++)
+    {
+        for (int col = 0; col < columna; col++)
+        {
+            cout << elementos[fil][col] << " ";
+        }
+        cout << endl;
+    }
 }
+
+// inline Matriz::~Matriz()
+// {
+//     cout << " Destruyendo matriz " << endl;
+// }
